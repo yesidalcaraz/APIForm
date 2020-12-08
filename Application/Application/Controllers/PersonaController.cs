0d4ff621c8094a4c01cb4021d51ca63314b789b0
Yesid Alcaraz <catadorverde@gmail.com>
2020-12-09 00:47:30 +0100

    APIForm
    Una API para hacer cositas cheveres

---
+++ Application/Application/Controllers/PersonaController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Application.Controllers
+{
+    public class PersonaController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Add(Models.Request.PersonaRequest model)
+        {
+            using(Models.pruebillafelizEntities2 db = new Models.pruebillafelizEntities2())
+            {
+                var oPersona = new Models.personas();
+                oPersona.nombre = model.Nombre;
+                oPersona.edad = model.Edad;
+                db.personas.Add(oPersona);
+                db.SaveChanges();
+
+            }
+
+            return Ok("Exito");
+
+
+        }
+    }
+}
---
+++ Application/Application/Models/Request/PersonaRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Application.Models.Request
+{
+    public class PersonaRequest
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+    }
+}
